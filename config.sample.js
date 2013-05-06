be25f7560e68aa7139464f8fda03be6efbf08886
xuyannan <xyn0563@gmail.com>
2013-05-06 12:28:29 +0800

    增加twitter数据源

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+exports.config = function(){
+    return {
+        twitter : {
+          consumer_key: 'STATE YOUR NAME',
+          consumer_secret: 'STATE YOUR NAME',
+          access_token_key: 'STATE YOUR NAME',
+          access_token_secret: 'STATE YOUR NAME'
+        }
+    };
+}
